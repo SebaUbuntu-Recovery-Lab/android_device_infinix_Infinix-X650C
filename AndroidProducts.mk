a5da3e2a6227001e25b724d612256d28a55b2dfe
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-09-29 20:33:45 +0200

    Infinix-X650C: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: infinix Model: Infinix X650C Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Infinix-X650C.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Infinix-X650C-user \
+    omni_Infinix-X650C-userdebug \
+    omni_Infinix-X650C-eng
